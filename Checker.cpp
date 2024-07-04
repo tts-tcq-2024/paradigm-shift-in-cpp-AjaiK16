10fc808a7589644478f40e9027676e7ba48890eb
AjaiK16 <ajaikannank16@gmail.com>
2024-07-04 12:20:09 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,24 +2,40 @@
 #include <iostream>
 using namespace std;
 
-bool isWithinRange(float value, float min, float max, std::string Message) {
-    if (value < min || value > max) {
-        cout << Message << endl;
+bool isWithinRange(float value, float min, float max) {
+    return value >= min && value <= max;
+}
+
+bool isBelowMax(float value, float max) {
+    return value <= max;
+}
+
+bool checkRangeAndReport(float value, float min, float max, const char* errorMessage) {
+    if (!isWithinRange(value, min, max)) {
+        cout << errorMessage << endl;
+        return false;
+    }
+    return true;
+}
+
+bool checkMaxAndReport(float value, float max, const char* errorMessage) {
+    if (!isBelowMax(value, max)) {
+        cout << errorMessage << endl;
         return false;
     }
     return true;
 }
 
 bool isTemperatureOk(float temperature) {
-    return isWithinRange(temperature, 0, 45, "Temperature out of range!");
+    return checkRangeAndReport(temperature, 0, 45, "Temperature out of range!");
 }
 
 bool isSocOk(float soc) {
-    return isWithinRange(soc, 20, 80, "State of Charge out of range!");
+    return checkRangeAndReport(soc, 20, 80, "State of Charge out of range!");
 }
 
 bool isChargeRateOk(float chargeRate) {
-    return isWithinRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
+    return checkMaxAndReport(chargeRate, 0.8, "Charge Rate out of range!");
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
@@ -27,11 +43,14 @@ bool batteryIsOk(float temperature, float soc, float chargeRate) {
 }
 
 int main() {
-    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(25, 70, 0.7) == true); 
     assert(batteryIsOk(50, 70, 0.7) == false);
     assert(batteryIsOk(25, 85, 0.7) == false);
     assert(batteryIsOk(25, 70, 0.9) == false);
     assert(batteryIsOk(-10, 70, 0.7) == false);
-    assert(batteryIsOk(25, 10, 0.7) == false);
+    assert(batteryIsOk(25, 10, 0.7) == false); 
+
+    cout << "All tests passed!\n";
     return 0;
 }
+
