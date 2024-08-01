8c7bfba7b6d8fc91277549be947883270477dbc9
AjaiK16 <ajaikannank16@gmail.com>
2024-08-01 14:59:30 +0530

    Update and rename Checker.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+#include "bms.h"
+
+void testBatteryManagementSystem() {
+    BatteryManagementSystem bms;
+
+    bms.setLanguage(Language::ENGLISH);
+
+    assert(bms.batteryIsOk(25, 70, 0.7) == true);
+    assert(bms.batteryIsOk(50, 70, 0.7) == false);
+    assert(bms.batteryIsOk(25, 85, 0.7) == false);
+    assert(bms.batteryIsOk(25, 70, 0.9) == false);
+    assert(bms.batteryIsOk(-10, 70, 0.7) == false);
+    assert(bms.batteryIsOk(25, 10, 0.7) == false);
+
+    bms.setLanguage(Language::GERMAN);
+
+    assert(bms.batteryIsOk(25, 70, 0.7) == true);
+    assert(bms.batteryIsOk(50, 70, 0.7) == false);
+    assert(bms.batteryIsOk(25, 85, 0.7) == false);
+    assert(bms.batteryIsOk(25, 70, 0.9) == false);
+    assert(bms.batteryIsOk(-10, 70, 0.7) == false);
+    assert(bms.batteryIsOk(25, 10, 0.7) == false);
+}
+
+int main() {
+    testBatteryManagementSystem();
+    return 0;
+}
